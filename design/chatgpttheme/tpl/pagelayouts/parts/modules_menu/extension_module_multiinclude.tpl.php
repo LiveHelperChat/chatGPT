d10aa38f5744b2893ef0c3ceebdb428d621d9da9
Remigijus Kiminas <remdex@gmail.com>
2025-04-23 08:17:31 -0400

    Rename to OpenAI

--- design/chatgpttheme/tpl/pagelayouts/parts/modules_menu/extension_module_multiinclude.tpl.php
+++ design/chatgpttheme/tpl/pagelayouts/parts/modules_menu/extension_module_multiinclude.tpl.php
@@ -1,3 +1,3 @@
 <?php if (erLhcoreClassUser::instance()->hasAccessTo('lhchatgpt','configure')) : ?>
-    <li class="nav-item"><a class="nav-link" href="<?php echo erLhcoreClassDesign::baseurl('chatgpt/index')?>"><i class="material-icons">integration_instructions</i><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chatgpt/module','ChatGPT');?></a></li>
+    <li class="nav-item"><a class="nav-link" href="<?php echo erLhcoreClassDesign::baseurl('chatgpt/index')?>"><i class="material-icons">integration_instructions</i><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chatgpt/module','OpenAI');?></a></li>
 <?php endif; ?>
